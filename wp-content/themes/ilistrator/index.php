7a71e76ca1981442576f1551541c9d7f4a03b588
root <ilincapsc@gmail.com>
2018-03-04 23:28:02 -0800

    Playing with content.

--- wp-content/themes/ilistrator/index.php
+++ wp-content/themes/ilistrator/index.php
@@ -5,10 +5,11 @@
       while(have_posts()): the_post(); ?>
 
         <h3><?php the_title(); ?></h3>
+
         <small>
           Posted on: <?php the_time('F j, Y'); ?>, 
-          at <?php the_time('g:i a'); ?> 
-          <?php the_category(); ?>
+          at         <?php the_time('g:i a'); ?> 
+                     <?php the_category(); ?>
         </small>
         
         <p><?php the_content(); ?></p>
